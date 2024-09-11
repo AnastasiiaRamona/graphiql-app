607d35c9200c636da5a8e17025bf29326e23ee28
Oleksandr <aleks6699@gmail.com>
2024-09-11 21:29:45 +0300

    fix: RSS-FINAL-7_GraphQL-tests fix formatData.test.tsx

--- graphiql-app/src/test/formatData.test.tsx
+++ graphiql-app/src/test/formatData.test.tsx
@@ -1,16 +1,17 @@
-import { describe, it, expect } from 'vitest';
+import { parseISO, format } from 'date-fns';
 import formatDate from '@/utils/formatDate';
+
 describe('formatDate', () => {
   it('formats a date string correctly', () => {
     const inputDate = '2024-09-11T15:30:00Z';
-    const expectedOutput = '11.09.2024 18:30';
+    const expectedOutput = format(parseISO(inputDate), 'dd.MM.yyyy HH:mm');
 
     expect(formatDate(inputDate)).toBe(expectedOutput);
   });
 
   it('returns a formatted date even for edge cases', () => {
     const inputDate = '2020-02-29T12:00:00Z';
-    const expectedOutput = '29.02.2020 14:00';
+    const expectedOutput = format(parseISO(inputDate), 'dd.MM.yyyy HH:mm');
 
     expect(formatDate(inputDate)).toBe(expectedOutput);
   });
