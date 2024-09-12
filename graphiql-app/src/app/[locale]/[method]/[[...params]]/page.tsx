01e922857ff3310f68c763e1ad24bf3b486c3ef7
Anastasia <anastasia@MacBook-Air-Anastasia.local>
2024-09-12 10:04:54 +0200

    style: fix mb of Box

--- graphiql-app/src/app/[locale]/[method]/[[...params]]/page.tsx
+++ graphiql-app/src/app/[locale]/[method]/[[...params]]/page.tsx
@@ -259,6 +259,7 @@ function RestfullForm() {
                   sx={{
                     display: 'flex',
                     justifyContent: 'flex-start',
+                    mt: 1,
                   }}
                 >
                   <PrettyButton
