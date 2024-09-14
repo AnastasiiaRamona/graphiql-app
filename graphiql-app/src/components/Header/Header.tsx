5fc98cd0066b99ad935e9c71486893740e0dfe66
Anastasia <anastasia@MacBook-Air-Anastasia.local>
2024-09-14 21:33:52 +0200

    fix: change mainIcon format

--- graphiql-app/src/components/Header/Header.tsx
+++ graphiql-app/src/components/Header/Header.tsx
@@ -18,7 +18,7 @@ import Link from 'next/link';
 import MaterialUISwitch from '../SwitchTheme/MaterialUiSwitch';
 import SwitchLanguage from '../SwitchLanguage/SwitchLanguage';
 import Props from './types';
-import iconSrc from '../../assets/mainIcon.ico';
+import iconSrc from '../../assets/mainIcon.webp';
 import useHeaderNavigation from '@/hooks/useHeaderNavigation';
 import useAuthStore from '@/store/store';
 import { onAuthStateChanged } from 'firebase/auth';
